dae7285ca9b99a7c368a925e9fb158237180b17b
Monazir Muhammad Doha <getmonazirnow@gmail.com>
2023-10-15 02:47:45 +0600

    set cookie to remember credentials

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
-from flask import Flask,request,render_template,redirect, make_response
+from flask import Flask,request,render_template, make_response
 from assist import User
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    r = render_template("index.html")
+    if request.cookies.get("creds"):
+        r = render_template("logged_in.html")
+    else:
+        r = render_template("index.html")
     return r
 @app.route("/data",methods=["POST"])
 def data():
     try:
+        if request.cookies.get("creds"):
+            cookie = request.cookies.get("creds")
+            creds = cookie.split(":")
+            reg = creds[0]
+            password = creds[1]
+        else:
+            reg = request.form["reg"]
+            password = request.form["password"]
+
         url = request.form["url"]
-        reg = request.form["reg"]
-        password = request.form["password"]
         target = User(url,reg,password)
         valid = target.login()
         if valid:
+            response = make_response()
+            response.set_cookie("creds",f"{reg}:{password}",domain=request.host)
             url = target.getVidLink()
-            return render_template("redirect.html",link=url) 
+            response.content_type = "text/html"
+            response.data = render_template("redirect.html",link = url)
+            return response 
         else:
-            return render_template("invalid.html") 
+            response = make_response()
+            response.delete_cookie("creds")
+            response.data = render_template("invalid.html")
+            response.content_type = "text/html"
+            return response
     except:
         return render_template("invalid.html") 
 
